021109b5d6994d382d59d3d70525b5582398df66
ofuji0602 <hitujimofumofu0206@gmail.com>
2024-08-29 11:23:31 +0900

    掲示板の編集、削除機能の実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,6 @@
 class BoardsController < ApplicationController
+  # :edit, :update, :destroyアクションの前に必ず呼び出されるメソッドを設定
+  before_action :find_board, only: [ :edit, :update, :destroy ]
   # 一覧表示アクション
   def index
     # 全てのボードを取得し、ユーザー情報も一緒に読み込む
@@ -31,10 +33,42 @@ class BoardsController < ApplicationController
     @comments = @board.comments.includes(:user).order(created_at: :desc)
   end
 
+  def edit
+    @board = current_user.boards.find(params[:id])
+  end
+
+  # ボードを更新するアクション
+  def update
+    # find_boardメソッドでセットされた@boardを更新
+    if @board.update(board_params)
+      # 更新に成功した場合、@boardの詳細ページにリダイレクトし、成功メッセージを表示
+      redirect_to @board, success: t("defaults.message.updated", item: Board.model_name.human)
+    else
+      # 更新に失敗した場合、エラーメッセージをフラッシュで表示し、editページを再レンダリング
+      flash.now["danger"] = t("defaults.message.not_updated", item: Board.model_name.human)
+      render :edit
+    end
+  end
+
+  # ボードを削除するアクション
+  def destroy
+    # find_boardメソッドでセットされた@boardを削除
+    @board.destroy!
+    # 削除後、ボード一覧ページにリダイレクトし、削除成功メッセージを表示
+    redirect_to boards_path, success: t("defaults.message.deleted", item: Board.model_name.human)
+  end
+
   private
 
+  # 特定のボードを現在のユーザーのボードから見つけ、@boardにセットするメソッド
+  def find_board
+    @board = current_user.boards.find(params[:id])
+  end
+
   # ボードのパラメータを許可するメソッド
+  private
   def board_params
+    # params.require(:board)でboardパラメータを取得し、その中で許可するパラメータを指定
     params.require(:board).permit(:title, :body, :board_image, :board_image_cache)
   end
 end
