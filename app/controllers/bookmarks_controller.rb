4d79a829a1b2ca955035e2f65f911836605c262d
ofuji0602 <hitujimofumofu0206@gmail.com>
2024-09-01 04:25:41 +0900

    ブックマークボタンのajax化

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,15 +1,11 @@
 class BookmarksController < ApplicationController
   def create
-    board = Board.find(params[:board_id])
-    current_user.bookmark(board)
-    flash[:success] = t("bookmarks.create.success")
-    redirect_to request.referer || root_path
+    @board = Board.find(params[:board_id])
+    current_user.bookmark(@board)
   end
 
   def destroy
-    board = current_user.bookmarks.find(params[:id]).board
-    current_user.unbookmark(board)
-    flash[:success] = t("bookmarks.destroy.success")
-    redirect_to request.referer || root_path
+    @board = current_user.bookmarks.find(params[:id]).board
+    current_user.unbookmark(@board)
   end
 end
