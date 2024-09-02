5cc1b288878f1de5e7f6a542831a7c02aa65dd43
ofuji0602 <hitujimofumofu0206@gmail.com>
2024-09-03 05:55:03 +0900

    プロフィール編集機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_29_050248) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_02_201025) do
   create_table "boards", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_29_050248) do
     t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
